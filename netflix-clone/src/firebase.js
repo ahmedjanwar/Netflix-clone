df00df53c8ab720c32b27b03bc408abef8544652
ahmedjanwar <ajemal840@gamil.com>
2022-10-15 22:42:22 +0300

    initial init

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAjFHT_nQ4PChhiQ02dpn1kRQjtg2YYfME",
+    authDomain: "netflix-clone-6aaec.firebaseapp.com",
+    projectId: "netflix-clone-6aaec",
+    storageBucket: "netflix-clone-6aaec.appspot.com",
+    messagingSenderId: "517066079227",
+    appId: "1:517066079227:web:b06457b617add2870c695e"
+  };
